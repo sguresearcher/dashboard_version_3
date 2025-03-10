ba1f3c10d7981737207cb8e8c59820df18f952e1
Duta Nugraha <duta.nugraha82@gmail.com>
2025-03-10 19:02:55 +0700

    add auth in api

--- app/Http/Controllers/GuestCT.php
+++ app/Http/Controllers/GuestCT.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Http;
 class GuestCT extends Controller
 {
     public function index(){
-        $response = Http::get("http://10.20.100.172:7777/data/telkom/conpot/24h");
+        // $response = Http::get("http://10.20.100.172:7777/data/telkom/conpot/24h");
+        $response = Http::withBasicAuth('S6ur3searcher!#', 'pleasechangeme123')->get("http://10.20.100.172:7777/data/telkom/conpot/24h");
 
         if ($response->successful()) {
             $data = $response->json();
