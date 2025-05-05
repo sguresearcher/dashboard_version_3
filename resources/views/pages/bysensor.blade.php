4b51a57874f83da5321a61a2eb6ad3324b9602b0
Duta Nugraha <duta.nugraha82@gmail.com>
2025-05-05 21:51:47 +0700

    fixing bysensor blade v8

--- resources/views/pages/bysensor.blade.php
+++ resources/views/pages/bysensor.blade.php
@@ -48,7 +48,7 @@
         <div class="card border-0">
             <div class="card-body">
                 <h6 class="mb-4">Top 10 Attacker IP</h6>
-                <canvas id="top10IpTenant" height="200px"></canvas>
+                <canvas id="top10IpTenant" style="height: 200px; width:100%"></canvas>
             </div>
         </div>
     </div>
